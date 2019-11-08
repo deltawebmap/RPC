d31b6488be002d6349437104184277b3723bc296
Roman-Port <darkwings220@gmail.com>
2019-11-08 16:45:35 -0600

    Add project files.

---
+++ DeltaUserGateway/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeltaUserGateway
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
